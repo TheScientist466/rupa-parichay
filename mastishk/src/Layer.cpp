98b59f1415f97585da4e6b1d36d79bbf18b485d9
parthib0019 <parthib2290352@gmail.com>
2025-11-05 09:25:26 +0530

    feature: added mastishk

---
+++ mastishk/src/Layer.cpp
@@ -0,0 +1,7 @@
+#include "mastishk/Layer.hpp"
+#include <iostream>
+
+int foo() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
--- pradarshanam/src/main.cpp
+++ pradarshanam/src/main.cpp
@@ -1,6 +1,9 @@
 #include "renderer.hpp"
 
+#include <mastishk/Layer.hpp>
+
 int main() {
+    foo();
     Renderer renderer;
     return renderer.run();
 }
